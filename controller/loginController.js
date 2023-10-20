40524a1dcc51e921f85a06850a430ea54a334d4c
amiramar1368 <a.amarloo68@gmail.com>
2023-10-20 13:43:49 +0330

    change login controller to adoupt 10.30

--- controller/loginController.js
+++ controller/loginController.js
@@ -9,9 +9,12 @@ export class Login{
             return res.render("login",{error:"نام کاربری و رمز عبور الزامی می باشد",layout:"./layout/loginLayout.ejs"})
         }
         try {
-            const {data} = await axios.post("http://192.168.10.20/api/login",{login,password});
+            // const {data} = await axios.post("http://192.168.10.20/api/login",{login,password});
+            const device_name = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+            const { data } = await axios.post("http://192.168.10.20/users/login", { login, password,device_name });
         if(!data.message){
-        process.env.token = data.user.token;
+        // process.env.token = data.user.token;
+        process.env.token = data.access_token;
         process.env.full_name = data.user.full_name;        
          res.render("home",{error:""})
         }else{
