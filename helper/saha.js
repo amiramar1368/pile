331ef17da6562d6f253d608f68c4939b43b6ec80
hbagloo <hbagloo@gmail.com>
2024-01-18 14:56:04 +0330

    edit saha to cr report

--- helper/saha.js
+++ helper/saha.js
@@ -2,10 +2,20 @@ import axios from 'axios';
 
 export async function saha_r(start_at,end_at,config){
     try {
-    const {data:saha_E} = await axios.post(`http://192.168.10.20/report/weighbridge/w2`,{start_at,end_at,destination_id:1,weighbridge_id:6},config);
-    const {data:saha_W} = await axios.post(`http://192.168.10.20/report/weighbridge/w2`,{start_at,end_at,destination_id:1,weighbridge_id:7},config);
-    const all_service = [...saha_E,...saha_W]
-    return all_service;
+
+        const {data} = await axios.post(`http://192.168.10.20/report/weighbridge/w2`,{
+        start_at,
+        end_at,
+        destination_id:1,
+        weighbridge_id:null,
+        type:1
+    },config);
+
+    // const {data:saha_E} = await axios.post(`http://192.168.10.20/report/weighbridge/w2`,{start_at,end_at,destination_id:1,weighbridge_id:6},config);
+    // const {data:saha_W} = await axios.post(`http://192.168.10.20/report/weighbridge/w2`,{start_at,end_at,destination_id:1,weighbridge_id:7},config);
+    // const all_service = [...saha_E,...saha_W]
+    // return all_service;
+    return data;
 
     } catch (err) {
         console.log(err);
